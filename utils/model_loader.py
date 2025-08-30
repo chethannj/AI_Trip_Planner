22d4b2b251819401a5f0716d56a6990c9d10bfbc
chethannj <53956942+chethannj@users.noreply.github.com>
2025-08-30 21:49:47 +0530

    Update model_loader.py

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field
 from utils.config_loader import load_config
 from langchain_groq import ChatGroq
 from langchain_openai import ChatOpenAI
-import streamlit as st
 
 
 class ConfigLoader:
@@ -34,14 +33,13 @@ class ModelLoader(BaseModel):
         print(f"Loading model from provider: {self.model_provider}")
         if self.model_provider == "groq":
             print("Loading LLM from Groq..............")
-            groq_api_key = st.secrets("GROQ_API_KEY")
+            groq_api_key = os.getenv("GROQ_API_KEY")
             model_name = self.config["llm"]["groq"]["model_name"]
             llm=ChatGroq(model=model_name, api_key=groq_api_key)
         elif self.model_provider == "openai":
             print("Loading LLM from OpenAI..............")
-            openai_api_key = st.secrets("OPENAI_API_KEY")
+            openai_api_key = os.getenv("OPENAI_API_KEY")
             model_name = self.config["llm"]["openai"]["model_name"]
             llm = ChatOpenAI(model_name="o4-mini", api_key=openai_api_key)
         
         return llm
-    
